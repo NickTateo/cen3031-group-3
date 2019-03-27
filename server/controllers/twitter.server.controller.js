84a6797bcb9a82a2c6f59800769231681c980353
mmacdonald <37754975+TheAngriestDog@users.noreply.github.com>
2019-03-27 16:55:39 -0400

    Smoothed Twitter controller process
    Modified the controller process to have less function calls and act a little more directly.

--- server/controllers/twitter.server.controller.js
+++ server/controllers/twitter.server.controller.js
@@ -9,57 +9,34 @@ var client = new Twitter({
     access_token_secret: 'EAQsACn8tR1RVKVLOjrg6hx3nbDw5Aa0sDdqoXMaYYW3J'
 });
 
-function checkValidArea(userInput){
-    //var woeid;
-    //var twitterResponse;
-    let promise = new Promise(function(resolve, reject){
-        client.get('trends/available', function(error, response){
+function checkValidArea(userInput) {
+    let promise = new Promise(function (resolve, reject) {
+        client.get('trends/available', function (error, response) {
             if (error) {
                 console.log(error);
                 reject(error);
                 throw error;
             }
-            //twitterResponse = response;
-            resolve(response);
+            for (let i = 0; i < response.length; i++) {
+                if (response[i].name == userInput) {
+                    console.log("found woeid is: " + response[i].woeid);
+                    resolve(response[i].woeid);
+                }
+            };
+            resolve(-1);
         });
-    });
+
+    })
     return promise;
 }
 
-    //promise.then(result => successCallBack(result));
-
-async function successCallBack(result){
-    var final = -1;
-    await checkValidArea(result).then(twitterResponse => {
-        //console.log(JSON.stringify(twitterResponse));
-        for (let i = 0; i < twitterResponse.length; i++) {
-            //console.log(twitterResponse[i].name);
-            if (twitterResponse[i].name == result) {
-                console.log("found woeid is: " + twitterResponse[i].woeid);
-                final = twitterResponse[i].woeid;
-                return final;
-                //return woeid;            
-            }
-        }
-    });
-    
-    return final;
-}
-    
-    // console.log("woeid: " + woeid);
-    // return woeid;
-
-
 
 exports.test = (req, res, next) => {
-    // client.get('trends/place', {id: '2450022'}, function(error, tweets, response) {
     return client.get('trends/place', { id: '615702' }, function (error, response) {
         if (error) {
             console.log(error);
             throw error;
         }
-        // console.log(tweets);  // The favorites.
-        // console.log(response);  // Raw response object.
         twitter_response = response;
         console.log("got info from twitter");
 
@@ -78,45 +55,38 @@ exports.test = (req, res, next) => {
 
         return res.status(200).json(twitter_response);
     });
-    // return res.status(200).write(JSON.stringify(twitter_response));
-    // res.write(JSON.stringify(twitter_response));
 }
 
 
-exports.dynamicTrends = async function(req, res, next){
-    var woeid = await successCallBack(req.params.userPlace);
-    console.log("woeid is: " + woeid);
-    var userPlace;
-    if (woeid != -1) {
-        return client.get('trends/place', { id: woeid }, function (error, response) {
-            if (error) {
-                console.log(error);
-                throw error;
-            }
-            // console.log(tweets);  // The favorites.
-            // console.log(response);  // Raw response object.
-            twitter_response = response;
-            console.log("got info from twitter");
-            //console.log(JSON.stringify(twitter_response));
-            function sortTrends(tweet_volume) {
-                return function (a, b) {
-                    if (a[tweet_volume] < b[tweet_volume]) {
-                        return 1;
-                    }
-                    else if (a[tweet_volume] > b[tweet_volume]) {
-                        return -1;
-                    }
-                    return 0;
+exports.dynamicTrends = function (req, res, next) {
+    checkValidArea(req.params.userPlace).then(woeid => {
+        console.log("Result is " + woeid);
+        if (woeid != -1) {
+            client.get('trends/place', { id: woeid}, function (error, response) {
+                if (error) {
+                    console.log(error);
+                    throw error;
                 }
-            }
-            twitter_response[0].trends.sort(sortTrends("tweet_volume"));
-
-            return res.status(200).json(twitter_response);
-        });
-    }
-    else {
-        return res.status(200).json("Sorry, That location is either not trending or is not valid.");
-    }
-    console.log("in server controller looking at " + req.params.userPlace);
-    return res.status(200);
+                twitter_response = response;
+                console.log("got info from twitter");
+                function sortTrends(tweet_volume) {
+                    return function (a, b) {
+                        if (a[tweet_volume] < b[tweet_volume]) {
+                            return 1;
+                        }
+                        else if (a[tweet_volume] > b[tweet_volume]) {
+                            return -1;
+                        }
+                        return 0;
+                    }
+                }
+                twitter_response[0].trends.sort(sortTrends("tweet_volume"));
+    
+                return res.status(200).json(twitter_response);
+            });
+        }
+        else {
+            return res.status(200).json("Sorry, That location is either not trending or is not valid.");
+        }
+    });
 }
