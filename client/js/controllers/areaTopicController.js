bf1b2e29f4682a03a8b7ce5551c1e980c69b6325
ffore <daniel.geleroff@gmail.com>
2019-03-30 17:19:13 -0400

    top tweets by favorites and error if no session storage

--- client/js/controllers/areaTopicController.js
+++ client/js/controllers/areaTopicController.js
@@ -4,9 +4,15 @@ angular.module('twitter').controller('areaTopicController', ['$scope', 'Twitter'
 	var place = sessionStorage.getItem('place');
 	var topic = sessionStorage.getItem('topic');
 
+	if(!place || !topic){
+		console.log("no session storage!");
+		return;
+	}
+
     Twitter.areaTopic(place, topic).then((response)=>{
         console.log("Called this on init with values " + place + " and " + topic);
-        //console.log(response);
+        // console.log(response.data.statuses[0].user.name + "'s tweet got " + response.data.statuses[0].favorite_count + " favorites");
+    	topTweetsGraph(response);
     });
 
 
@@ -20,6 +26,92 @@ angular.module('twitter').controller('areaTopicController', ['$scope', 'Twitter'
             }
             return 0;
         }
+    }
+
+    function topTweetsGraph(response){
+    	response.data.statuses.sort(sorting("favorite_count"));
+    	var labelName = [], labelPop = [];
+    	var tweetAmount = response.data.statuses.length; 
+    	var firstZero = -1;
+    	if(tweetAmount >= 10){
+    		tweetAmount = 10;
+    		for(var i = 0; i < 10; i++){
+    			// labelName[i] = "#"+i;
+    			labelPop[i] = response.data.statuses[i].favorite_count;
+    			if(firstZero == -1 && response.data.statuses[i].favorite_count == 0){
+    				firstZero = i;
+    			}
+    		}
+    	}
+    	else{
+    		for(var i = 0; i < tweetAmount; i++){
+    			// labelName[i] = "#"+i;
+    			labelPop[i] = response.data.statuses[i].favorite_count;
+    			if(firstZero == -1 && response.data.statuses[i].favorite_count == 0){
+    				firstZero = i;
+    			}
+    		}
+    	}
+
+    	console.log(labelPop);
+    	console.log(labelName);
+    	console.log("first zero: " + firstZero);
+
+    	if(firstZero == 0){
+    		console.log("No top tweets");
+    		return;
+    	}
+    	if(firstZero != -1){
+    		for(var i = 0; i < firstZero; i++){
+    			var num = i + 1;
+    			labelName[i] = "#" + num;
+    		}
+    	}
+    	else{
+    		for(var i = 0; i < tweetAmount; i++){
+    			var num = i + 1;
+    			labelName[i] = "#" + num;
+    		}
+    	}
+
+    	var ctx = document.getElementById('top-tweets').getContext('2d');
+    	chart = new Chart(ctx, {
+    		// The type of chart we want to create
+            type: 'bar',
+
+           // The data for our dataset
+            data: {
+              labels: labelName,
+              datasets: [{
+                label: '',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: labelPop,
+                display: true
+              }]
+            },
+
+            // Configuration options go here
+            options: {
+              scales: {
+                xAxes: [{
+                  scaleLabel :
+                  {
+                    display: true,
+                    labelString: "Top Tweets"
+                  }
+                }],
+                yAxes: [{
+                  scaleLabel : 
+                  {
+                    display:true,
+                    labelString: "# of Favorites"
+                  }
+                }]
+              }
+            }
+    	});
+
     }
 }
 ]);
